20f5ba77a278a0abefe84b02539582030e91f8bb
Zer0fuqqs <tomhe0905@gmail.com>
2022-12-12 18:08:06 +0100

    Loading Kreis hinzugefügt Data script angepasst

--- data.js
+++ data.js
@@ -17,25 +17,48 @@ async function getMonthlyStockData(API_KEY, Symbol) {
     let data = await fetch(path).then(res => res.json());
 
     // Looped über die Daten und filtert auf das Jahr 2022
+    // Try..catch für Errorhandling falls ein API Call fehlschlägt
+    try {
+        for(const [key, value] of Object.entries(data["Monthly Adjusted Time Series"])) {
 
-    for(const [key, value] of Object.entries(data["Monthly Adjusted Time Series"])) {
-
-        if(key > "2021-12-31") {
-            data2022[key] = value
+            if(key > "2021-12-31") {
+                data2022[key] = value
+            }
         }
+    } catch(e) {
+        alert("Stock couldnt be find. Please check if your symbol is right!")
+        throw new Error("Something went badly wrong!");
     }
 
     return data2022
 }
 
+async function ShowSpinner() {
+    let SpinnerDiv = document.createElement('div');
+    SpinnerDiv.setAttribute('id', 'Spinner')
+    SpinnerDiv.classList.add('justify-content-center')
+    SpinnerDiv.classList.add('d-flex')
+    SpinnerDiv.innerHTML='<div class="spinner-border" style="width: 3rem; height: 3rem; margin-top: 260px;" role="status"> <span class="visually-hidden">Loading...</span> </div </div>'
+
+    let parent = document.getElementById("ChartDiv")
+    parent.parentNode.insertBefore(SpinnerDiv, parent)
 
 
-async function createDiagramm() { 
-    
+}
+
+function DeleteSpinnerAndShowDiagramm(ChartElement, labels, prices, symbol) {
+    document.getElementById('Spinner').remove();
+    DrawDiagramm(ChartElement, labels, prices, symbol);
+}
+
+
+
+async function GetData() { 
+
+    ShowSpinner()
+
     // Check ob schon ein Chart erstellt wurde, da der canvas mit dem chart immer leer sein muss 
     // um ein neuen Chart zu generieren
-    
-
     if( Object.keys(Chart.instances).length !== 0) {
         let ChartNumber = Object.keys(Chart.instances)[0];
         Chart.instances[`${ChartNumber}`].destroy()
@@ -44,53 +67,59 @@ async function createDiagramm() {
     // Das eingegebene Symbol vom Input Feld
     let symbol = document.getElementById("symbolInput").value
     // Chart Canvas
-    const ctx = document.getElementById('myChart');
-
+    const ChartElement = document.getElementById('myChart');
 
     let data2022 = await getMonthlyStockData(API_KEY, symbol)
-    let labels = Object.keys(data2022)
+    let labels = [];
     let prices = [];
 
-    for(const [key, value] of Object.entries(data2022)) {
+    Object.keys(data2022).forEach(element => {
+        labels.push(new Date(element).toLocaleString('en-de',{month:'short', year:'numeric'}))
+    });
 
-    prices.push(value["4. close"])
-    }
-
+        for(const [key, value] of Object.entries(data2022)) {
+            prices.push(value["4. close"])
+        }
     
 
-    const chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-        data: {
-        labels: labels.reverse(),
-        datasets: [{
-            label: `Wertentwicklung 2022 ${symbol} (1 Year | $ )`,
-            data: prices.reverse(),
-            tension:"0.2"
-        }]
-        
-    },
-    // Configuration options go here
-    options: {
-        responsive: false,
-        legend: {
-        labels: {
-            color: "white"
-        }
-            
-        },
-        scales: {
-        y: {
-            ticks: { color: 'white', beginAtZero: true },
-            grid: { display: false, borderColor: 'white' }
-            
-        },
-        x: {
-            ticks: { color: 'white', beginAtZero: true },
-            grid: { display: false, borderColor: 'white' }
-            }
-        }
-        }
-    })
+    setTimeout( () => {DeleteSpinnerAndShowDiagramm(ChartElement, labels, prices, symbol)}, 800); 
 }
 
+
+
+async function DrawDiagramm(ChartElement, labels, prices, symbol) {
+    const chart = new Chart(ChartElement, {
+        // The type of chart we want to create
+        type: 'line',
+            data: {
+            labels: labels.reverse(),
+            datasets: [{
+                label: `Wertentwicklung 2022 ${symbol} (1 Year | $ )`,
+                data: prices.reverse(),
+                tension:"0.2"
+            }]
+            
+        },
+        // Configuration options go here
+        options: {
+            responsive: false,
+            legend: {
+            labels: {
+                color: "white"
+            }
+                
+            },
+            scales: {
+            y: {
+                ticks: { color: 'white', beginAtZero: true },
+                grid: { display: false, borderColor: 'white' }
+                
+            },
+            x: {
+                ticks: { color: 'white', beginAtZero: true },
+                grid: { display: false, borderColor: 'white' }
+                }
+            }
+            }
+        })
+}
